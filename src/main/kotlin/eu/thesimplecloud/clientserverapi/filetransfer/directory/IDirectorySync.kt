c45b85ec6b59cf56af060ece58f3729929f2a844
Frederick <frederick.baier.berlin@outlook.de>
2019-10-02 19:56:03 +0200

    Added comments to the IDirectorySync interface

--- src/main/kotlin/eu/thesimplecloud/clientserverapi/filetransfer/directory/IDirectorySync.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/filetransfer/directory/IDirectorySync.kt
@@ -5,10 +5,21 @@ import java.io.File
 
 interface IDirectorySync {
 
+    /**
+     * Returns the directory to sync from
+     */
     fun getDirectory(): File
 
+    /**
+     * Synchronizes the content of the directory with the content of the directory on the other side.
+     * It also will update all changes to the specified connection
+     */
     fun syncDirectory(connection: IConnection)
 
+    /**
+     * Removes the connection from the list with connections to sync the directory to.
+     * The connection will no longer receive updates of this directory.
+     */
     fun syncNoLonger(connection: IConnection)
 
 }
