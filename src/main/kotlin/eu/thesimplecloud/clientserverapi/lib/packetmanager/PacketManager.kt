9e0c14bb1c5e38e9a26fd475101b41fe40926bb9
Frederick <frederick.baier.berlin@outlook.de>
2020-01-01 12:53:24 +0100

    Marked methods in PacketManager.kt with synchronized. Changed version to 2.0.10.

--- src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packetmanager/PacketManager.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packetmanager/PacketManager.kt
@@ -10,12 +10,14 @@ import kotlin.collections.HashMap
 
 class PacketManager : IPacketManager {
 
-    private val packets = Maps.newConcurrentMap<Int, Class<out IPacket>>()
+    private val packets = HashMap<Int, Class<out IPacket>>()
 
-    private val packetRegisterCallbacks = Collections.synchronizedCollection(ArrayList<Pair<Class<out IPacket>, CompletableFuture<Int>>>())
+    private val packetRegisterCallbacks = ArrayList<Pair<Class<out IPacket>, CompletableFuture<Int>>>()
 
+    @Synchronized
     override fun getPacketClassById(id: Int) = packets[id]
 
+    @Synchronized
     fun registerPacket(id: Int, packetClass: Class<out IPacket>) {
         packets[id] = packetClass
         val callbacks = packetRegisterCallbacks.filter { it.first == packetClass }
@@ -23,22 +25,27 @@ class PacketManager : IPacketManager {
         packetRegisterCallbacks.removeAll(callbacks)
     }
 
+    @Synchronized
     override fun getIdFromPacket(packet: IPacket): Int? = getIdFromPacket(packet::class.java)
 
+    @Synchronized
     override fun getIdFromPacket(packetClass: Class<out IPacket>): Int? = packets.entries.firstOrNull { it.value == packetClass }?.key
 
     fun clearPackets() = this.packets.clear()
 
+    @Synchronized
     fun getUnusedId(): Int {
         var id = 0
         while (packets.contains(id)) id++
         return id
     }
 
+    @Synchronized
     override fun getPacketClassByName(name: String) = packets.values.firstOrNull {
         it.simpleName == name
     }
 
+    @Synchronized
     fun <T : IPacket> getPacketIdCompletableFuture(packetClass: Class<T>): CompletableFuture<Int> {
         val completableFuture = CompletableFuture<Int>()
         val id = getIdFromPacket(packetClass)
