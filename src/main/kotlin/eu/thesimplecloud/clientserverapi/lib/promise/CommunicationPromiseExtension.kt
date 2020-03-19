c943b0f4891e01bf1cb3d6e6e9b2d99384334f44
Frederick <frederick.baier.berlin@outlook.de>
2020-03-19 10:36:12 +0100

    Added descriptions to the functions in CommunicationPromiseExtension.kt and a parameter to specify the additional timeout. Changed version to 2.2.1-SNAPSHOT.

--- src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/promise/CommunicationPromiseExtension.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/promise/CommunicationPromiseExtension.kt
@@ -31,10 +31,17 @@ fun <T : Any> ICommunicationPromise<out ICommunicationPromise<T>>.flatten(additi
     return newPromise
 }
 
+/**
+ * Returns a [ICommunicationPromise] with a list. The returned promise will complete when all promises in this list are completed.
+ */
 fun <T : Any> List<ICommunicationPromise<T>>.toListPromise(): ICommunicationPromise<List<T>> {
     return this.combineAllPromises().then { this.map { it.getNow() } }
 }
 
-fun <T : Any> ICommunicationPromise<List<ICommunicationPromise<T>>>.toListPromise(): ICommunicationPromise<List<T>> {
-    return this.then { list -> list.toListPromise()  }.flatten(400)
+/**
+ * Returns a [ICommunicationPromise] with a list. The returned promise will complete when all promises in this list are completed.
+ * Note: if the expected list is large it is recommended to increase the [additionalTimeout]. Otherwise the returned promise will just time out.
+ */
+fun <T : Any> ICommunicationPromise<List<ICommunicationPromise<T>>>.toListPromise(additionalTimeout: Long = 400): ICommunicationPromise<List<T>> {
+    return this.then { list -> list.toListPromise()  }.flatten(additionalTimeout)
 }
