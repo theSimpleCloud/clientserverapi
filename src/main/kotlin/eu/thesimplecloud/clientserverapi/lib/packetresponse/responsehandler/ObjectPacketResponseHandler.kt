a6de98a391714d19ce8d3925a9ea27d408eee9a8
Frederick <baier.frederick@gmail.com>
2020-05-07 18:17:29 +0200

    Added a debug message. Changed version to 2.3.26-SNAPSHOT.

--- src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packetresponse/responsehandler/ObjectPacketResponseHandler.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packetresponse/responsehandler/ObjectPacketResponseHandler.kt
@@ -2,9 +2,6 @@ package eu.thesimplecloud.clientserverapi.lib.packetresponse.responsehandler
 
 import eu.thesimplecloud.clientserverapi.lib.packet.IPacket
 import eu.thesimplecloud.clientserverapi.lib.packet.packettype.ObjectPacket
-import java.lang.IllegalArgumentException
-import java.lang.IllegalStateException
-import kotlin.reflect.KClass
 
 open class ObjectPacketResponseHandler<T : Any>(private val expectedClass: Class<T>) : IPacketResponseHandler<T> {
 
@@ -20,6 +17,7 @@ open class ObjectPacketResponseHandler<T : Any>(private val expectedClass: Class
         val valueClass: Class<out T>? = packet.value?.let { it::class.java }
         if (valueClass != null && !Throwable::class.java.isAssignableFrom(valueClass)) {
             if (expectedClass == Unit::class.java) return Unit as T
+            if (!expectedClass.isAssignableFrom(valueClass)) println("The class of the value does not match the expected one: expected ${expectedClass.name} was ${valueClass.name}")
             if (!expectedClass.isAssignableFrom(valueClass)) throw IllegalStateException("The class of the value does not match the expected one: expected ${expectedClass.name} was ${valueClass.name}")
         }
         return packet.value
