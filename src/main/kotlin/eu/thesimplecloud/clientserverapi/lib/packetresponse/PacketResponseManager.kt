c7551702cc40a386e3cb9f91cd7d02de4a22ef0d
Frederick <baier.frederick@gmail.com>
2020-05-13 21:35:32 +0200

    Added a debug messages. Changed version to 2.3.31-SNAPSHOT.

--- src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packetresponse/PacketResponseManager.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packetresponse/PacketResponseManager.kt
@@ -1,6 +1,7 @@
 package eu.thesimplecloud.clientserverapi.lib.packetresponse
 
 import com.google.common.collect.Maps
+import eu.thesimplecloud.clientserverapi.lib.LogFile
 import eu.thesimplecloud.clientserverapi.lib.packet.WrappedPacket
 import eu.thesimplecloud.clientserverapi.lib.promise.ICommunicationPromise
 import java.util.*
@@ -23,15 +24,15 @@ class PacketResponseManager : IPacketResponseManager {
 
     @Synchronized
     fun incomingPacket(wrappedPacket: WrappedPacket) {
-        println("incoming packet " + wrappedPacket.packetData.sentPacketName + " (${wrappedPacket.packetData.uniqueId})")
+        LogFile.INSTANCE.addMessage("incoming packet " + wrappedPacket.packetData.sentPacketName + " (${wrappedPacket.packetData.uniqueId})")
         val wrappedResponseHandler: WrappedResponseHandler<out Any>? = packetResponseHandlers.remove(wrappedPacket.packetData.uniqueId)
-        println("response handler for ${wrappedPacket.packetData.sentPacketName}: $wrappedResponseHandler (${wrappedPacket.packetData.uniqueId})")
+        LogFile.INSTANCE.addMessage("response handler for ${wrappedPacket.packetData.sentPacketName}: $wrappedResponseHandler (${wrappedPacket.packetData.uniqueId})")
         wrappedResponseHandler
                 ?: throw IllegalStateException("Incoming: No response handler was available for packet by id ${wrappedPacket.packetData.uniqueId}")
         val response = wrappedResponseHandler.packetResponseHandler.handleResponse(wrappedPacket.packet)
         val packetPromise = wrappedResponseHandler.communicationPromise
-        println("found promise $packetPromise | response: $response | ${response?.let { it::class }} (${wrappedPacket.packetData.uniqueId})")
-        println("throwable (${wrappedPacket.packetData.uniqueId}) ${response is Throwable}")
+        LogFile.INSTANCE.addMessage("found promise $packetPromise | response: $response | ${response?.let { it::class }} (${wrappedPacket.packetData.uniqueId})")
+        LogFile.INSTANCE.addMessage("throwable (${wrappedPacket.packetData.uniqueId}) ${response is Throwable}")
         packetPromise as ICommunicationPromise<Any>
         if (response is Throwable) {
             packetPromise.tryFailure(response)
