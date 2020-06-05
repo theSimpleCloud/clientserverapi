0d58f1c2726101957bb60f8da784ffd2463d470c
Frederick <baier.frederick@gmail.com>
2020-06-05 15:03:27 +0200

    Fix bug synchronizing directories. Add copyright. Change version to 2.4.13-SNAPSHOT.

--- src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/filetransfer/directory/DirectorySync.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/filetransfer/directory/DirectorySync.kt
@@ -1,3 +1,25 @@
+/*
+ * MIT License
+ *
+ * Copyright (C) 2020 Frederick Baier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 package eu.thesimplecloud.clientserverapi.lib.filetransfer.directory
 
 import eu.thesimplecloud.clientserverapi.lib.connection.IConnection
@@ -111,7 +133,7 @@ class DirectorySync(private val directory: File, toDirectory: String, private va
     private fun shouldSendViaZip(allFiles: List<File>, neededFiles: List<File>): Boolean {
         val allFilesSize = allFiles.map { it.length() }.sum()
         val neededFilesSize = neededFiles.map { it.length() }.sum()
-        return (allFilesSize / 5) < neededFilesSize
+        return (allFilesSize / 3) < neededFilesSize
     }
 
     private fun syncDirectories(connection: IConnection) {
--- src/main/kotlin/eu/thesimplecloud/clientserverapi/server/client/connectedclient/ConnectedClient.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/server/client/connectedclient/ConnectedClient.kt
@@ -1,10 +1,32 @@
+/*
+ * MIT License
+ *
+ * Copyright (C) 2020 Frederick Baier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 package eu.thesimplecloud.clientserverapi.server.client.connectedclient
 
 import eu.thesimplecloud.clientserverapi.lib.bootstrap.ICommunicationBootstrap
-import io.netty.channel.Channel
 import eu.thesimplecloud.clientserverapi.lib.connection.AbstractConnection
 import eu.thesimplecloud.clientserverapi.server.INettyServer
 import eu.thesimplecloud.clientserverapi.server.NettyServer
+import io.netty.channel.Channel
 
 class ConnectedClient<T : IConnectedClientValue>(private val channel: Channel, private val nettyServer: NettyServer<T>) : AbstractConnection(nettyServer.packetManager, nettyServer.packetResponseManager), IConnectedClient<T> {
 
