93430d064317b7469eddb547b08b520ddb7d386e
Frederick <baier.frederick@gmail.com>
2020-11-30 14:27:10 +0100

    Remove asynchronous query sending methods

--- src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packet/packetsender/IPacketSender.kt
+++ src/main/kotlin/eu/thesimplecloud/clientserverapi/lib/packet/packetsender/IPacketSender.kt
@@ -23,9 +23,7 @@
 package eu.thesimplecloud.clientserverapi.lib.packet.packetsender
 
 import eu.thesimplecloud.clientserverapi.lib.packet.IPacket
-import eu.thesimplecloud.clientserverapi.lib.promise.CommunicationPromise
 import eu.thesimplecloud.clientserverapi.lib.promise.ICommunicationPromise
-import eu.thesimplecloud.clientserverapi.lib.promise.flatten
 
 interface IPacketSender {
 
@@ -36,24 +34,10 @@ interface IPacketSender {
     fun <T : Any> sendQuery(packet: IPacket, expectedResponseClass: Class<T>, timeout: Long = 200): ICommunicationPromise<T>
 
     /**
-     * Sends a query asynchronously to the connection and returns a [ICommunicationPromise].
-     * @return a [ICommunicationPromise] to wait for the result.
-     */
-    fun <T : Any> sendQueryAsync(packet: IPacket, expectedResponseClass: Class<T>, timeout: Long = 200): ICommunicationPromise<T> {
-        return CommunicationPromise.runAsync { sendQuery(packet, expectedResponseClass, timeout) }.flatten()
-    }
-
-    /**
      * Sends a query to the connection and returns a [ICommunicationPromise].
      * @return a [ICommunicationPromise] to wait for the result.
      */
     fun sendUnitQuery(packet: IPacket, timeout: Long = 200): ICommunicationPromise<Unit> = sendQuery(packet, Unit::class.java, timeout)
-
-    /**
-     * Sends a query asynchronously to the connection and returns a [ICommunicationPromise].
-     * @return a [ICommunicationPromise] to wait for the result.
-     */
-    fun sendUnitQueryAsync(packet: IPacket, timeout: Long = 200): ICommunicationPromise<Unit> = sendQueryAsync(packet, Unit::class.java, timeout)
 
 }
 
@@ -62,9 +46,3 @@ interface IPacketSender {
  * @return a [ICommunicationPromise] to wait for the result.
  */
 inline fun <reified T : Any> IPacketSender.sendQuery(packet: IPacket, timeout: Long = 200): ICommunicationPromise<T> = sendQuery(packet, T::class.java, timeout)
-
-/**
- * Sends a query asynchronously to the connection and returns a [ICommunicationPromise].
- * @return a [ICommunicationPromise] to wait for the result.
- */
-inline fun <reified T : Any> IPacketSender.sendQueryAsync(packet: IPacket, timeout: Long = 200): ICommunicationPromise<T> = sendQueryAsync(packet, T::class.java, timeout)
